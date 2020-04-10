1b4296238201a9a2a7dd90535a0bc61744b8a332
Jeremiah Risinger <risinger.jeremiah@gmail.com>
2020-04-10 14:24:17 -0500

    proper camera movemetn

--- Assets/LookingDirection.cs
+++ Assets/LookingDirection.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class LookingDirection : MonoBehaviour
 {
     public float speedH = 2.0f;
@@ -9,6 +10,8 @@ public class LookingDirection : MonoBehaviour
 
     public float yaw = 0.0f;
     public float pitch = 0.0f;
+
+    [SerializeField] Transform player;
 
     // Start is called before the first frame update
     void Start()
@@ -22,7 +25,9 @@ public class LookingDirection : MonoBehaviour
         yaw += speedH * Input.GetAxis("Mouse X");
         pitch -= speedV * Input.GetAxis("Mouse Y");
 
+        player.eulerAngles = new Vector3(0.0f, yaw, 0.0f);
         transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);
+
     }
 
 }
