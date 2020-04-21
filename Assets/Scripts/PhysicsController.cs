bc6b5e20310f5ccd45b7bb2a095cbdfe238cdf78
flyguy-595 <56926768+flyguy-595@users.noreply.github.com>
2020-04-21 17:50:38 -0500

    updated movement code
    - fixed bug allowing moving through walls
    - fixed bug where you would bounce against walls
    - fixed bug where player velcocity would change when running into a wall or enemy

---
+++ Assets/Scripts/PhysicsController.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhysicsController : MonoBehaviour
+{
+
+    [SerializeField] float wallDistance = .5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public bool IsGrounded()
+    {
+
+        Vector3 left = new Vector3(transform.position.x - 0.25f, transform.position.y, transform.position.z);
+        Vector3 right = new Vector3(transform.position.x + 0.25f, transform.position.y, transform.position.z);
+
+        if (Physics.Raycast(transform.position, Vector3.down, 0.3f) || Physics.Raycast(left, Vector3.down, 0.3f) || Physics.Raycast(right, Vector3.down, 0.3f))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public bool IsLeftWall() {
+        Vector3 bottom = new Vector3(transform.position.x - 0.25f, transform.position.y, transform.position.z);
+        Vector3 top = new Vector3(transform.position.x + 0.25f, transform.position.y, transform.position.z);
+
+        if (Physics.Raycast(transform.position, -transform.right, wallDistance) || Physics.Raycast(bottom, -transform.right, wallDistance) || Physics.Raycast(top, -transform.right, wallDistance))
+        {
+            Debug.Log("Left Wall");
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public bool IsRightWall() {
+        Vector3 bottom = new Vector3(transform.position.x, transform.position.y - 0.25f, transform.position.z);
+        Vector3 top = new Vector3(transform.position.x, transform.position.y + 0.25f, transform.position.z);
+
+        if (Physics.Raycast(transform.position, transform.right, wallDistance) || Physics.Raycast(bottom, transform.right, wallDistance) || Physics.Raycast(top, transform.right, wallDistance))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+
+    }
+
+    public bool IsBackWall()
+    {
+        Vector3 topRight = new Vector3(transform.position.x + 0.25f, transform.position.y + 0.25f, transform.position.z);
+        Vector3 topLeft = new Vector3(transform.position.x - 0.25f, transform.position.y + 0.25f, transform.position.z);
+        Vector3 bottomRight = new Vector3(transform.position.x + 0.25f, transform.position.y - 0.25f, transform.position.z);
+        Vector3 bottomLeft = new Vector3(transform.position.x - 0.25f, transform.position.y - 0.25f, transform.position.z);
+
+        if (Physics.Raycast(topRight, -transform.forward, wallDistance) || Physics.Raycast(topLeft, -transform.forward, wallDistance) || Physics.Raycast(bottomRight, -transform.forward, wallDistance) || Physics.Raycast(bottomLeft, -transform.forward, wallDistance))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+
+    }
+
+    public bool IsFrontWall()
+    {
+        Vector3 topRight = new Vector3(transform.position.x + 0.25f, transform.position.y + 0.25f, transform.position.z);
+        Vector3 topLeft = new Vector3(transform.position.x - 0.25f, transform.position.y + 0.25f, transform.position.z);
+        Vector3 bottomRight = new Vector3(transform.position.x + 0.25f, transform.position.y - 0.25f, transform.position.z);
+        Vector3 bottomLeft = new Vector3(transform.position.x - 0.25f, transform.position.y - 0.25f, transform.position.z);
+
+        if (Physics.Raycast(topRight, transform.forward, wallDistance) || Physics.Raycast(topLeft, transform.forward, wallDistance) || Physics.Raycast(bottomRight, transform.forward, wallDistance) || Physics.Raycast(bottomLeft, transform.forward, wallDistance))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+
+    }
+
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,50 +3,120 @@ using System;
 
 public class PlayerController : MonoBehaviour
 {
-	public Rigidbody rb;
-	public Transform t;
+	Rigidbody rb;
 	public float degrees;
 	public float zOffset;
 	public float xOffset;
+    PhysicsController physicsController;
 
-	public Collider head;
-	public Collider body;
-	public Collider mesh;
+    //public Collider head;
+	//public Collider body;
+	//public Collider mesh;
 	public float health = 500.0f;
 	public float speed = .1f;
-	
-	//[SerializeField] float eulerAngX;
-	////[SerializeField] float eulerAngY;
-	//[SerializeField] float eulerAngZ;
-	void Update()
-	{
-		//eulerAngX = transform.localRotation.eulerAngles.x;
-		//eulerAngY = transform.localRotation.eulerAngles.y;
-		//eulerAngZ = transform.localRotation.eulerAngles.z;// transform.localEulerAngles.z;
-		degrees = transform.localRotation.eulerAngles.y;
-		zOffset = speed * (float)Math.Cos((degrees) / 180 * Math.PI);
-		xOffset = speed * (float)Math.Sin((degrees) / 180 * Math.PI);
+    [SerializeField] float jumpSpeed = 1f;
 
-		if (Input.GetKey(KeyCode.W))
+    //[SerializeField] float eulerAngX;
+    ////[SerializeField] float eulerAngY;
+    //[SerializeField] float eulerAngZ;
+
+    void Start()
+    {
+        physicsController = GetComponent<PhysicsController>();
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+	{
+        //eulerAngX = transform.localRotation.eulerAngles.x;
+        //eulerAngY = transform.localRotation.eulerAngles.y;
+        //eulerAngZ = transform.localRotation.eulerAngles.z;// transform.localEulerAngles.z;
+        degrees = transform.localRotation.eulerAngles.y;
+        zOffset = speed * (float)Math.Cos((degrees) / 180 * Math.PI);
+        xOffset = speed * (float)Math.Sin((degrees) / 180 * Math.PI);
+
+        /*if (physicsController.IsGrounded() && rb.velocity.magnitude < 0.005)
+        {
+            if (Input.GetButton("Jump"))
+            {
+                applyForce = true;
+            }
+        }
+        else
+        {
+            applyForce = false;
+        }*/
+
+        /**if (physicsController.IsXNegWall())
+        {
+                xOffset = 0f;
+        }
+
+        if (physicsController.IsXPosWall())
+        {
+                xOffset = 0f;
+        }
+
+        if (physicsController.IsZNegWall())
+        {
+                zOffset = 0f;
+        }
+
+        if (physicsController.IsZPosWall())
+        {
+                zOffset = 0f;
+        }**/
+
+        //transform.position = new Vector3(transform.position.x + (moveVector.x), transform.position.y, transform.position.z + (moveVector.z));
+
+        Debug.Log("X Offset: " + xOffset + ", Z Offset: " + zOffset);
+       // Debug.Log("forward: " + transform.forward);
+
+        if (Input.GetKey(KeyCode.W))
 		{
-			t.position = new Vector3(transform.position.x + xOffset, transform.position.y, transform.position.z + zOffset);
+            if (physicsController.IsFrontWall())
+            {
+                xOffset = 0;
+                zOffset = 0;
+            }
+            transform.position = new Vector3(transform.position.x + xOffset, transform.position.y, transform.position.z + zOffset);
 		}
 		if (Input.GetKey(KeyCode.S))
 		{
-			t.position = new Vector3(transform.position.x - xOffset, transform.position.y, transform.position.z - zOffset);
+            if (physicsController.IsBackWall()) {
+                xOffset = 0;
+                zOffset = 0;
+            }
+            transform.position = new Vector3(transform.position.x - xOffset, transform.position.y, transform.position.z - zOffset);
 		}
-		if (Input.GetKey(KeyCode.D))
-		{
-			t.position = new Vector3(transform.position.x + zOffset, transform.position.y, transform.position.z - xOffset);
-		}
+        if (Input.GetKey(KeyCode.D))
+        {
+            if (physicsController.IsRightWall()) {
+                xOffset = 0;
+                zOffset = 0;
+            }
+            transform.position = new Vector3(transform.position.x + zOffset, transform.position.y, transform.position.z - xOffset);
+        }
 		if (Input.GetKey(KeyCode.A))
 		{
-			t.position = new Vector3(transform.position.x - zOffset, transform.position.y, transform.position.z + xOffset);
+            if (physicsController.IsLeftWall()) {
+                xOffset = 0;
+                zOffset = 0;
+            }
+            transform.position = new Vector3(transform.position.x - zOffset, transform.position.y, transform.position.z + xOffset);
 		}
 
 		if (health <= 0)
 		{
-			gameObject.SetActive(false);
+			//gameObject.SetActive(false);
 		}
+
+        rb.velocity = Vector3.zero;
 	}
+
+    private void FixedUpdate()
+    {
+        
+    }
+
 }
