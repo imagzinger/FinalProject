f88eb3b410d874d95058244fd2cfc5647a58a7eb
Jeremiah Risinger <risinger.jeremiah@gmail.com>
2020-05-05 14:39:13 -0500

    reseting

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,10 +1,8 @@
 ﻿using UnityEngine;
 using System;
-using System.Diagnostics;
 
 public class PlayerController : MonoBehaviour
 {
-
 	public Rigidbody rb;
 	public Transform t;
 	public float degrees;
@@ -16,23 +14,18 @@ public class PlayerController : MonoBehaviour
 	public Collider mesh;
 	public float health = 500.0f;
 	public float speed = .1f;
-
 	public bool hasObjective = false;
-	[SerializeField] Camera cam;
-	[SerializeField] GameObject crossHair;
-	[SerializeField] GameObject snipeHair;
-	private bool sniping = false;
-
-	float scroll = 1;
-	[SerializeField] float zoomSpeed  = 100.0f; 
+	//[SerializeField] float eulerAngX;
+	////[SerializeField] float eulerAngY;
+	//[SerializeField] float eulerAngZ;
 	void Update()
 	{
+		//eulerAngX = transform.localRotation.eulerAngles.x;
+		//eulerAngY = transform.localRotation.eulerAngles.y;
+		//eulerAngZ = transform.localRotation.eulerAngles.z;// transform.localEulerAngles.z;
 		degrees = transform.localRotation.eulerAngles.y;
 		zOffset = speed * (float)Math.Cos((degrees) / 180 * Math.PI);
 		xOffset = speed * (float)Math.Sin((degrees) / 180 * Math.PI);
-		scroll = Input.GetAxis("Mouse ScrollWheel") *50;
-		cam.fieldOfView -= scroll;
-		//UnityEngine.Debug.LogError(scroll);
 
 		if (Input.GetKey(KeyCode.W))
 		{
@@ -50,24 +43,6 @@ public class PlayerController : MonoBehaviour
 		{
 			t.position = new Vector3(transform.position.x - zOffset, transform.position.y, transform.position.z + xOffset);
 		}
-		if (Input.GetKey(KeyCode.Space))
-		{
-			//t.position = new Vector3(transform.position.x - zOffset, transform.position.y, transform.position.z + xOffset);
-			rb.AddForce(0.0f,10.0f,0.0f,ForceMode.VelocityChange);
-		}
-		//if (scroll > 0.0f)
-		//{
-		//	sniping = true;
-		//	snipeHair.SetActive(sniping);
-		//	crossHair.SetActive(!sniping);
-		//}
-		//if (scroll <= 0.0f)
-		//{
-		//	scroll = 0.0f;
-		//	sniping = false;
-		//	snipeHair.SetActive(sniping);
-		//	crossHair.SetActive(!sniping);
-		//}
 
 		if (health <= 0)
 		{
