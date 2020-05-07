07fc6a70141defaa2daa02bd96138c5aef6f60af
Jeremiah Risinger <risinger.jeremiah@gmail.com>
2020-05-06 23:51:52 -0500

    AI respawn at objective

--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -1,20 +1,28 @@
 ﻿
+using System.Security.Cryptography;
 using UnityEngine;
 
 public class Target : MonoBehaviour
 {
 
     public float health = 100f;
-
+    [SerializeField] Transform objective;
     public void TakeDamage(float amount) {
 
         health = health - amount;
         if (health <= 0f) {
 
             //DieAnimation();
-            Destroy(gameObject);
+            gameObject.SetActive(false);
+            Invoke("SpawnNew",10f);
         }
 
+    }
+
+    void SpawnNew()
+    {
+        gameObject.transform.position = objective.position;
+        gameObject.SetActive(true);
     }
 
 }
